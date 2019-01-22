151aaa2d650ae3c285d9e0ac8ada81c8733d208d
Alex Semyonov <alex@semyonov.us>
2019-01-22 09:41:55 +0300

    Provide OpenAPI specs by common names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Graphiti::OpenAPI::Engine.routes.draw do
   resources :specifications, only: :index, defaults: {format: :html}
 
+  get "openapi.:format" => "specifications#index", defaults: {format: :json}
   root to: "specifications#index", defaults: {format: :html}
 end
