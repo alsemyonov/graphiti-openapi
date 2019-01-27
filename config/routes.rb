1965e64457a9470c682376df882905f03bba558e
Alex Semyonov <alex@semyonov.us>
2019-01-28 02:46:17 +0300

    Add named route for OpenAPI spec files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Graphiti::OpenAPI::Engine.routes.draw do
   resources :specifications, only: :index, defaults: {format: :html}
 
-  get "openapi.:format" => "specifications#index", defaults: {format: :json}
+  get "openapi.:format" => "specifications#index", defaults: {format: :json}, as: :openapi
   root to: "specifications#index", defaults: {format: :html}
 end
